c46aceec8fb284e75025a16d1eba6fe6796a55e0
gustavodetoni <gustavodetoni2407@gmail.com>
2024-11-02 02:00:43 -0300

    fix: routers

--- backend/src/api/routes/userRoutes.js
+++ backend/src/api/routes/userRoutes.js
@@ -43,6 +43,51 @@ router.post('/create', authenticateToken, authorizeRole('admin'), async (req, re
     }
 });
 
+router.put('/:id/edit', authenticateToken, authorizeRole('admin'), async (req, res) => {
+    const { id } = req.params;
+    const { username, password, role } = req.body;
+
+    if (!username && !password && !role) {
+        return res.status(400).json({ message: 'Pelo menos um campo deve ser fornecido para atualização' });
+    }
+
+    try {
+        const updates = {};
+        if (username) updates.username = username;
+        if (password) updates.password = await bcrypt.hash(password, 10);
+        if (role) updates.role = role;
+
+        const [result] = await pool.query('UPDATE users SET ? WHERE id = ?', [updates, id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+
+        res.json({ message: 'Usuário atualizado com sucesso' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erro ao atualizar usuário' });
+    }
+});
+
+
+router.delete('/:id/delete', authenticateToken, authorizeRole('admin'), async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const [result] = await pool.query('DELETE FROM users WHERE id = ?', [userId]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+
+        res.json({ message: 'Usuário excluído com sucesso' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erro ao excluir usuário' });
+    }
+});
+
 const getUsersByRole = async (role, res) => {
     try {
         const [users] = await pool.query(
